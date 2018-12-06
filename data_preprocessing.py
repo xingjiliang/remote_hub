4ddcdcfe2a8b6a3600fe3ae47c7c96723a2894f8
Xing Jiliang <1458103052@qq.com>
2018-12-06 11:45:25 +0800

    先跑起来再说

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -65,12 +65,12 @@ def generate_data(tsv_file, save_as_file=True):
             day_grained_8_days_data = np.array(day_grained_8_days_data, dtype='float64')
             day_growth_rate_vector = (np.array(day_grained_8_days_data[:, 4], dtype='float64') / float(day_grained_8_days_data[:, 4][0]) - 1).reshape(-1, 1)
             day_grained_8_days_data = np.concatenate([day_grained_8_days_data, day_growth_rate_vector], 1)
-            hour_grained_data = np.array(hour_grained_dataframe.iloc[i - 23:i + 1].values[:, 1:], dtype='float64')
+            hour_grained_data = np.array(hour_grained_dataframe.iloc[i - 24:i + 1].values[:, 1:], dtype='float64')
             hour_growth_rate_vector = (hour_grained_data[:, 1] / hour_grained_data[0, 1] - 1).reshape(-1, 1)
             data.append([hour_grained_dataframe.iloc[i].values[0],
                         day_grained_8_days_data[:-1],
                         day_grained_8_days_data[-1, :-1],
-                        np.concatenate([hour_grained_data, hour_growth_rate_vector], 1),
+                        np.concatenate([hour_grained_data, hour_growth_rate_vector], 1)[1:],
                         day_grained_8_days_data[-1, -1]]
                         )
     data = np.array(data)
